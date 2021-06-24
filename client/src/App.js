4527e917df079f7cc697b0ce1b4850f325aee5c3
Dane Enright <enrightco@comcast.net>
2021-06-24 09:55:28 -0600

    added NavTabs to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import NavBar from "./components/NavBar";
-import Bio from "./components/pages/bio";
-import Contact from "./components/pages/contact";
-import Home from "./components/pages/home";
-import Portfolio from "./components/pages/portfolio";
+import NavTabs from "./components/NavTabs";
+import Bio from "./components/bio";
+import Contact from "./components/contact";
+import Home from "./components/home";
+import Portfolio from "./components/portfolio";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="portfolio-app">
-      <NavBar />
-      <Home />
+      <NavTabs />
+      <Bio />
+      <Portfolio />
     </div>
   )
 
--- client/src/components/portfolio.js
+++ client/src/components/portfolio.js
@@ -4,15 +4,15 @@ function Portfolio() {
     const portfolio = [
      {
         id: 1,
-        image:"../../img/NightSky-Working-Screenshot.jpg",
+        image:"./img/NightSky-Working-Screenshot.jpg",
         title:"Night Sky",
-        description:"Really cool application that shows you where the International Space Station is currently located in real time. Also can enter your personal cordinates and see how far away it is from you at that exact moment!",
+        description:"Really cool application that shows you where the International Space Station is currently located in real time. Also can enter your personal coordinates and see how far away it is from you at that exact moment!",
         deploy:"https://cblock7.github.io/Night-Sky/",
         github:"https://github.com/Cblock7/Night-Sky",
     },
     {
         id: 2,
-        image:"../../img/Ford-Finder-2021-Working-Screenshot.jpg",
+        image:"./img/Ford-Finder-2021-Working-Screenshot.jpg",
         title:"Ford Finder 2021",
         description:"This application is very useful for any new customer shopping for a Brand New Ford Vehicle. When a user enter search criteria for a model of car or certain specifications they are presented with all the available options for 2021!",
         deploy:"https://whispering-fortress-31426.herokuapp.com/",
@@ -20,7 +20,7 @@ function Portfolio() {
     },
     {
         id: 3,
-        image:"../../img/budget-tracker-screenshot.jpg",
+        image:"./img/budget-tracker-screenshot.jpg",
         title:"Budget Tracker",
         description:"A useful application to keep track off a budget and expenses with offline capability to capture all user data.",
         deploy:"https://lit-mountain-41459.herokuapp.com/",
@@ -28,7 +28,7 @@ function Portfolio() {
     },
     {
         id: 4,
-        image:"../../img/fitness-tracker-screenshot.jpg",
+        image:"./img/fitness-tracker-screenshot.jpg",
         title:"Workout Tracker",
         description:"This application is very helpful for a user to enter their reps for a workout and keep track of their progress with their workouts.",
         deploy:"https://sleepy-ridge-27212.herokuapp.com/",
@@ -36,13 +36,13 @@ function Portfolio() {
     },
     {
         id: 5,
-        image:"../../img/Workday-Scheduler-Working-Screenshot.jpg",
+        image:"./img/Workday-Scheduler-Working-Screenshot.jpg",
         title:"Work Day Planner",
         description:"A helpful application to help some schedule their day and keep track of it.",
         deploy:"https://daneenright.github.io/APIs-Work-Day-Scheduler/",
         github:"https://github.com/DaneEnright/APIs-Work-Day-Scheduler",
     }   
-    ],
+    ];
    
 }
 
